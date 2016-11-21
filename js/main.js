998db4035dd31571bd41f2ad3a45f26f17de61d1
Richard Saunders <r.saunders@zengenti.com>
2016-11-21 20:17:44 +0000

    Updates to article tags

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+    $('.js-nav__toggle').on('click', function(e) {
+      $('.drawer').toggleClass('is-visible');
+    });
+
+    $('.js-drawer__close').on('click', function(e) {
+      $('.drawer').toggleClass('is-visible');
+    });
