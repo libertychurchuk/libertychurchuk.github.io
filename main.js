4411180a14a192876e9dea86a1e0c9b606a48892
Richard Saunders <mail@rdsaunders.co.uk>
2018-05-17 23:08:15 +0100

    Added privacy policy, cookie bar added links to footer, and off canvas menu.

--- main.js
+++ main.js
@@ -5,4 +5,5 @@
 {% include_relative js/vendor/fontfaceobserver.js %}
 {% include_relative js/vendor/accordion.js %}
 {% include_relative js/vendor/mediabox.js %}
+{% include_relative js/cookie-bar.js %}
 {% include_relative js/app.js %}
