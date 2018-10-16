05dee95873418019a030cc10cf1fa9ca3f2ba142
Richard Saunders <r.saunders@zengenti.com>
2018-10-16 17:24:44 +0100

    Updated url structure and cleanup of collections

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 ---
 ---
 
-{% include_relative js/vendor/fluidvids.min.js %}
-{% include_relative js/vendor/fontfaceobserver.js %}
-{% include_relative js/vendor/accordion.js %}
-{% include_relative js/vendor/mediabox.js %}
-{% include_relative js/cookie-bar.js %}
-{% include_relative js/app.js %}
+{% include_relative assets/js/vendor/fluidvids.min.js %}
+{% include_relative assets/js/vendor/fontfaceobserver.js %}
+{% include_relative assets/js/vendor/accordion.js %}
+{% include_relative assets/js/vendor/mediabox.js %}
+{% include_relative assets/js/cookie-bar.js %}
+{% include_relative assets/js/app.js %}
