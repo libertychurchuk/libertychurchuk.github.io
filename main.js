b3e77ef7ecd75a860f28c6bc7bd5e062ccf6e561
Richard Saunders <r.saunders@zengenti.com>
2019-04-03 17:18:21 +0100

    Added glide slider

--- main.js
+++ main.js
@@ -5,5 +5,6 @@
 {% include_relative assets/js/vendor/fontfaceobserver.js %}
 {% include_relative assets/js/vendor/accordion.js %}
 {% include_relative assets/js/vendor/mediabox.js %}
+{% include_relative assets/js/vendor/glide.js %}
 {% include_relative assets/js/cookie-bar.js %}
 {% include_relative assets/js/app.js %}
